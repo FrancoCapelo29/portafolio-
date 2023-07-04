2cb05318930adc0063325aad7d3084d482ce2ffb
Franco Capelo <115761575+FrancoCapelo29@users.noreply.github.com>
2023-07-04 15:57:02 -0500

    1

---
+++ port.js
@@ -0,0 +1,9 @@
+function redireccion1() {
+    window.location.href = "1ermomento/1er.html";
+}
+function redireccion2() {
+    window.location.href = "2domomento/2do.html";
+}
+function redireccion3() {
+    window.location.href = "3ermomento/3er.html";
+}

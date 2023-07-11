dedb6f3a00ae1d0689d2276d454595c30d85c978
Franco Capelo <115761575+FrancoCapelo29@users.noreply.github.com>
2023-07-11 09:42:00 -0500

    3

--- port.js
+++ port.js
@@ -1,9 +1,9 @@
 function redireccion1() {
-    window.location.href = "1ermomento/1er.html";
+    window.location.href = "1ermomento/index.html";
 }
 function redireccion2() {
-    window.location.href = "2domomento/2do.html";
+    window.location.href = "2domomento/index.html";
 }
 function redireccion3() {
-    window.location.href = "3ermomento/3er.html";
+    window.location.href = "3ermomento/index.html";
 }
